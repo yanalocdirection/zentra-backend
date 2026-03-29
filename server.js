c7c89b3f2f7db9b05583d5736f9238f01af72684
yanalocdirection <yanaloc.direction@gmail.com>
2026-03-29 18:44:49 -0300

    Update server.js

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ const pool = process.env.DATABASE_URL
   : new Pool({
       host: process.env.DB_HOST || "localhost",
       port: Number(process.env.DB_PORT || 5432),
-      database: process.env.DB_NAME || "zentra",
+      database: process.env.DB_NAME || "zentra_v2",
       user: process.env.DB_USER || "postgres",
       password: process.env.DB_PASSWORD || "",
     });
